f719ee4b96ea4674ccf4d431f9224eb6ab2ad3b6
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-03 15:35:28 +0700

    finished first task

---
+++ js/task-01.js
@@ -0,0 +1,37 @@
+const ulCategoriesRef = document.querySelector('ul');
+
+const ulNumberOfCategoriesRef = ulCategoriesRef.children;
+console.log('Number of categories:', ulNumberOfCategoriesRef.length);
+
+// ------------------------------------------------------------------------------------------
+
+const firstCategoryRef = ulNumberOfCategoriesRef[0];
+
+const firstElementChildRef = firstCategoryRef.firstElementChild;
+console.log('Category:', firstElementChildRef.textContent);
+
+const lastElementChildRef = firstCategoryRef.lastElementChild;
+const amountElementsLastChild = lastElementChildRef.children;
+console.log('Elements:', amountElementsLastChild.length);
+
+// ----------------------------------------------------------------------------------------------
+
+const secondCategoryRef = ulNumberOfCategoriesRef[1];
+
+const secondElementChildRef = secondCategoryRef.firstElementChild;
+console.log('Category:', secondElementChildRef.textContent);
+
+const lastElementsChildRef = secondCategoryRef.lastElementChild;
+const amountElementLastChildRef = lastElementsChildRef.children;
+console.log('Elements:', amountElementLastChildRef.length);
+
+// ---------------------------------------------------------------------------------------
+
+const thirdCategoryEl = ulNumberOfCategoriesRef[2];
+
+const firstElementChildEl = thirdCategoryEl.firstElementChild;
+console.log('Category:', firstElementChildEl.textContent);
+
+const lastElementChildEl = thirdCategoryEl.lastElementChild;
+const amountElementsLastChildEl = lastElementChildEl.children;
+console.log('Elements:', amountElementsLastChildEl.length);
