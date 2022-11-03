abc4a2ab4549e3401a5dd059008efc6356c90421
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-03 20:07:43 +0700

    finished second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector('ul');
+
+const makeItemsList = items => {
+  return items.map(item => {
+    const liRef = document.createElement('li');
+  
+    liRef.classList.add('item');
+    liRef.textContent = item;
+  
+    return liRef;
+  });
+};
+
+const elements = makeItemsList(ingredients);
+ulRef.append(...elements);
