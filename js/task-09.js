a34a25de35c2b9fc900f3cdc16526b050f630b1c
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-05 21:04:42 +0700

    finished task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyRef = document.querySelector('body');
+const buttonRef = document.querySelector('.change-color');
+const nameOfBgColorRef = document.querySelector('.color');
+
+buttonRef.addEventListener('click', () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  nameOfBgColorRef.textContent = bodyRef.style.backgroundColor;
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
