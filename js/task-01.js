25cb1ee7884636dc22571ec0c2a22601956e0e00
Oleksandr  Vovk <vovk.sasha@outlook.com>
2022-11-07 19:36:01 +0700

    corrected task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,41 +1,9 @@
-//-------------------------------NUMBER OF CATEGORIES-----------------------------------
-const ulCategoriesRef = document.querySelector('ul');
+const listOfCategoriesRef = document.querySelector('ul');
+const itemsOfListRef = listOfCategoriesRef.children;
+console.log('Number of categories:', itemsOfListRef.length);
 
-const ulNumberOfCategoriesRef = ulCategoriesRef.children;
-console.log('Number of categories:', ulNumberOfCategoriesRef.length);
-
-
-// --------------------------------CATEGORY ANIMALS-------------------------------------
-
-const firstCategoryRef = ulNumberOfCategoriesRef[0];
-
-const firstElementChildRef = firstCategoryRef.firstElementChild;
-console.log('Category:', firstElementChildRef.textContent);
-
-const lastElementChildRef = firstCategoryRef.lastElementChild;
-const amountElementsLastChild = lastElementChildRef.children;
-console.log('Elements:', amountElementsLastChild.length);
-
-
-// --------------------------------CATEGORY PRODUCTS-------------------------------------
-
-const secondCategoryRef = ulNumberOfCategoriesRef[1];
-
-const secondElementChildRef = secondCategoryRef.firstElementChild;
-console.log('Category:', secondElementChildRef.textContent);
-
-const lastElementsChildRef = secondCategoryRef.lastElementChild;
-const amountElementLastChildRef = lastElementsChildRef.children;
-console.log('Elements:', amountElementLastChildRef.length);
-
-
-// --------------------------------CATEGORY TECHNOLOGIES----------------------------------
-
-const thirdCategoryEl = ulNumberOfCategoriesRef[2];
-
-const firstElementChildEl = thirdCategoryEl.firstElementChild;
-console.log('Category:', firstElementChildEl.textContent);
-
-const lastElementChildEl = thirdCategoryEl.lastElementChild;
-const amountElementsLastChildEl = lastElementChildEl.children;
-console.log('Elements:', amountElementsLastChildEl.length);
+for (const item of itemsOfListRef) {
+    console.log('Category:', item.firstElementChild.textContent);
+    const lastElementsChildRef = item.lastElementChild;
+    console.log('Elements:', lastElementsChildRef.children.length);
+};
